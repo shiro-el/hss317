df15a4901f675fcf3b67ddd3b56c6c570b4a5fd9
shiro-el <jae0406whan@gmail.com>
2025-06-04 01:35:07 +0900

    build

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   output: 'export',
+  assetPrefix: ".",
 };
 
 module.exports = nextConfig;
